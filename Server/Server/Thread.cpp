c88cc75c3de84c43527e3b2b57a8aa0445dc34d5
张在伟 <mikesimb@gmail.com>
2013-05-15 01:12:04 +0800

    增加2个单元

---
+++ Server/Server/Thread.cpp
@@ -0,0 +1,138 @@
+#include "StdAfx.h"
+#include "Thread.h"
+#include <Process.h>
+
+namespace TheOne
+{
+	CThread::CThread(void)
+	{
+		m_dwThreadID = 0 ;
+		m_hThread = INVALID_HANDLE_VALUE ;
+		m_bTerminated  = TRUE;
+		m_bIsSuspended = false;
+	}
+
+	CThread::~CThread(void)
+	{
+		if (m_hThread != INVALID_HANDLE_VALUE)
+		{
+			if (m_bIsSuspended) 
+			{
+				Resume();
+			}
+			Terminate();
+			DWORD time = MAX_WAIT_FOR_TIME;
+			WaitFor(time);
+		}
+
+	}
+
+	BOOL CThread::Start( BOOL bSuspended /*= FALSE */,unsigned int uStackSize /*= 0*/ )
+	{
+	   unsigned int dwThreadID;
+	   int iFlag = 0 ;
+	   if (bSuspended)
+	   {
+		   iFlag = CREATE_SUSPENDED;
+		   m_bIsSuspended = true;
+
+	   }
+	   else
+	   {
+		   m_bIsSuspended = false;
+	   }
+	   HANDLE hThread = (HANDLE)_beginthreadex(NULL ,uStackSize ,ThreadProc ,(void*)this ,iFlag ,&dwThreadID);
+
+	   if(hThread == INVALID_HANDLE_VALUE)
+		   return false;
+	   m_dwThreadID = dwThreadID;
+	   m_hThread = hThread;
+	   return true;
+
+	}
+
+	BOOL CThread::Suspend( void )
+	{
+		if ( SuspendThread(m_hThread) !=(DWORD) -1 )
+		{
+			m_bIsSuspended = TRUE;
+			return TRUE;
+		}
+		else
+		{
+			m_bIsSuspended = FALSE;
+			return FALSE;
+		}
+	}
+
+	BOOL CThread::Resume( void )
+	{
+	   if (ResumeThread(m_hThread) != (DWORD) -1)
+	   {
+		   m_bIsSuspended = FALSE;
+		   return TRUE;
+	   }
+	   else
+	   {
+		   m_bIsSuspended = TRUE;
+		   return  FALSE;
+	   }
+	}
+
+	void CThread::Terminate( void )
+	{
+		::InterlockedExchange((LONG *)(&m_bTerminated),TRUE);
+		_endthreadex(0);
+
+		
+	}
+
+	BOOL CThread::Terminated( void )
+	{
+	  return m_bTerminated ;
+	}
+
+	void CThread::WaitFor( const DWORD dwTimeOut /*= MAX_WAIT_FOR_TIME*/)
+	{
+	   if(m_hThread != INVALID_HANDLE_VALUE)
+	   {
+		   WaitForSingleObject(m_hThread,dwTimeOut);
+	   }
+
+	}
+
+	unsigned WINAPI CThread::ThreadProc( LPVOID pParam )
+	{
+	   CThread * pThead =(CThread *) pParam;
+	   ::InterlockedExchange((LONG *)&(pThead->m_bTerminated) , FALSE);
+	   try
+	   {
+
+
+		   pThead->Execute();
+
+	   }
+	   catch(...)
+	   {
+
+	   }
+	   try
+	   {
+		   pThead->OnTerminate();
+	   }
+	   catch(...) 
+	   {
+	   	
+	   }
+	   //CloseHandle(pThead->m_hThread);
+	   _endthreadex(0);
+	   pThead->m_hThread = INVALID_HANDLE_VALUE;
+	   pThead->m_dwThreadID = 0 ;
+	   pThead->m_bTerminated = true;
+	  // pThead->m_dwThreadID  = 0 ;
+	  // pThead->m_bTerminated = TRUE:
+	   return 0;
+	   
+	}
+
+}
---
+++ Server/Server/Thread.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <windows.h>
+
+#define MAX_WAIT_FOR_TIME 6000;
+
+namespace TheOne
+{
+   
+   const size_t THREAD_INFO_LEN = 32;
+
+class CThread
+{
+public:
+	CThread(void);
+	virtual ~CThread(void);
+	//启动线程
+    BOOL Start(BOOL bSuspended = FALSE ,unsigned int uStackSize = 0);
+
+	//线程挂起
+	BOOL Suspend(void);
+
+	//恢复线程
+	BOOL Resume(void);
+
+	//终止线程
+	void Terminate(void);
+
+	//等待线程
+	void WaitFor(const DWORD dwTimeOut /*= MAX_WAIT_FOR_TIME*/);
+
+	const char * getThreadID() {return (char *) m_dwThreadID;}
+	const char * getThreadHandle(){ return (char *)m_hThread;}
+
+
+protected:
+	//线程是否终止
+	BOOL Terminated(void);
+
+   //执行过程
+    virtual void Execute(void) = 0 ;
+   //终止事件
+	virtual void OnTerminate(void) { } ;
+
+	volatile BOOL m_bTerminated;
+
+private:
+    //线程ID
+	DWORD m_dwThreadID;
+    //线程句柄
+    HANDLE m_hThread;
+	//当前是否挂起
+	BOOL m_bIsSuspended;
+	//线程过程
+	static unsigned WINAPI ThreadProc(LPVOID pParam);
+
+
+
+};
+};
